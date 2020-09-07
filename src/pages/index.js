7cce80c6e27426e7e61ddd1334b91136dfe0a672
Harry Fitzgerald <harry.f@elanco.com>
2020-09-07 10:11:57 +0100

    init with tailwind

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
+		<p className="text-6xl text-center bg-blue-500 text-white">oiioi tailwind??</p>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
